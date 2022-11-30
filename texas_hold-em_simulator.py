0c01d22f73e448d9ff0d15c6702de742776b60b3
r0cd7b <tkdrbs1928@naver.com>
2022-11-30 17:39:30 +0900

    commit

--- texas_hold-em_simulator.py
+++ texas_hold-em_simulator.py
@@ -8,16 +8,17 @@ class Card:
         '4': (3,),
         '5': (4,),
         '6': (5,),
-        '7': (6,),
-        '8': (7,),
-        '9': (8,),
-        '10': (9,),
-        'J': (10,),
-        'Q': (11,),
-        'K': (12,),
+        # '7': (6,),
+        # '8': (7,),
+        # '9': (8,),
+        # '10': (9,),
+        # 'J': (10,),
+        # 'Q': (11,),
+        # 'K': (12,),
         'A': (0, 13)
     }
-    suits = {'♠': 0, '♣': 1, '♥': 2, '♦': 3}
+    # suits = {'♠': 0, '♣': 1, '♥': 2, '♦': 3}
+    suits = {'♠': 0, '♣': 1}
 
     def __init__(self):
         self.rank, self.suit = rank, suit
@@ -29,57 +30,37 @@ class Card:
 def seek_best(seven_cards_):
     # Straight flush 찾기
     seven_cards_.sort(key=lambda card_: (card_.suit, Card.ranks[card_.rank][-1]), reverse=True)
-    starting, stopping = 0, len(seven_cards_) - 5
+    best_five_ = [seven_cards_[0]]
     for i_ in range(1, len(seven_cards_)):
-        previous_i = i_ - 1
-        if Card.ranks[seven_cards_[previous_i].rank][-1] - 1 == Card.ranks[seven_cards_[i_].rank][-1] and \
-                seven_cards_[previous_i].suit == seven_cards_[i_].suit:
-            next_i = i_ + 1
-            if next_i - starting == 5:
-                return seven_cards_[starting:next_i], 'Straight flush'
-        elif i_ > stopping:
-            break
+        if seven_cards_[i_].suit == best_five_[-1].suit and \
+                Card.ranks[seven_cards_[i_].rank][-1] == Card.ranks[best_five_[-1].rank][-1] - 1:
+            best_five_.append(seven_cards_[i_])
+            if len(best_five_) == 5:
+                return best_five_, 'Straight flush'
+        elif i_ < 3:
+            best_five_.clear()
+            best_five_.append(seven_cards_[i_])
         else:
-            starting = i_
+            break
 
     # Lowest straight flush 찾기
     seven_cards_.sort(key=lambda card_: (card_.suit, Card.ranks[card_.rank][0]), reverse=True)
-    five = -1
+    best_five_.clear()
     for i_, card in enumerate(seven_cards_):
         if card.rank == '5':
-            if i_ > stopping:
-                break
-            five = i_
-        elif five > -1:
-            previous_i = i_ - 1
-            if Card.ranks[seven_cards_[previous_i].rank][0] - 1 == Card.ranks[seven_cards_[i_].rank][0] and \
-                    seven_cards_[previous_i].suit == seven_cards_[i_].suit:
-                next_i = i_ + 1
-                if next_i - five == 5:
-                    return seven_cards_[five:next_i], 'Straight flush'
-            elif i_ > stopping:
-                break
+            if i_ < 3:
+                best_five_.append(card)
             else:
-                five = -1
+                break
+        elif best_five_:
+            if card.suit == best_five_[-1].suit and Card.ranks[card.rank][0] == Card.ranks[best_five_[-1].rank][0] - 1:
+                best_five_.append(card)
+                if len(best_five_) == 5:
+                    return best_five_, 'Straight flush'
+            else:
+                best_five_.clear()
 
     # Four of a kind 찾기
-    seven_cards_.sort(key=lambda card_: (Card.ranks[card_.rank][-1], card_.suit), reverse=True)
-    starting, stopping = 0, len(seven_cards_) - 4
-    for i_ in range(1, len(seven_cards_)):
-        previous_i = i_ - 1
-        if seven_cards_[previous_i].rank == seven_cards_[i_].rank:
-            next_i = i_ + 1
-            if next_i - starting == 4:
-                if starting == 0:
-                    return seven_cards_[starting:starting + 5], 'Four of a kind'
-                else:
-                    best_five_ = seven_cards_[starting:starting + 4]
-                    best_five_.append(seven_cards_[0])
-                    return best_five_, 'Four of a kind'
-        elif i_ > stopping:
-            break
-        else:
-            starting = i_
 
     return seven_cards_[:5], 'High card'
 
@@ -118,7 +99,7 @@ hands = {
 }
 for i in range(players):
     seven_cards = hole[i] + community
-    print(seven_cards)
     best_five, hand = seek_best(seven_cards)
+    print(seven_cards)
     print(best_five, hand)
     print()
