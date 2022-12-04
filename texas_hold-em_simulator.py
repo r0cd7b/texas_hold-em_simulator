f1d3fed454183a6941e428e98a364b57a5e7e6df
r0cd7b <tkdrbs1928@naver.com>
2022-12-04 18:02:40 +0900

    commit

--- texas_hold-em_simulator.py
+++ texas_hold-em_simulator.py
@@ -5,13 +5,13 @@ from collections import deque
 
 class Card:
     """
-    ranks = {'A': (0, 13), '2': (1,), '3': (2,), '4': (3,), '5': (4,), '6': (5,), '7': (6,), '8': (7,), '9': (8,),
-             '10': (9,), 'J': (10,), 'Q': (11,), 'K': (12,)}
-    suits = {'♠': 0, '♣': 1, '♥': 2, '♦': 3}
+    ranks = {'2': (1,), '3': (2,), '4': (3,), '5': (4,), '6': (5,), '7': (6,), '8': (7,), '9': (8,), '10': (9,),
+             'J': (10,), 'Q': (11,), 'K': (12,), 'A': (0, 13)}
+    suits = ('♠', '♣', '♥', '♦')
     """
-    ranks = {'A': (0, 13), '2': (1,), '3': (2,), '4': (3,), '5': (4,), '6': (5,), '7': (6,), '8': (7,), '9': (8,),
-             '10': (9,), 'J': (10,), 'Q': (11,), 'K': (12,)}
-    suits = {'♠': 0, '♣': 1, '♥': 2, '♦': 3}
+    ranks = {'2': (1,), '3': (2,), '4': (3,), '5': (4,), '6': (5,), '7': (6,), '8': (7,), '9': (8,), '10': (9,),
+             'J': (10,), 'Q': (11,), 'K': (12,), 'A': (0, 13)}
+    suits = ('♠')
 
     def __init__(self, rank_, suit_):
         self.rank = rank_
@@ -21,66 +21,44 @@ class Card:
         return self.rank + self.suit
 
 
+def straight_flush(cards_, i):
+    j = 0
+    while Card.ranks[cards_[j].rank][i] > Card.ranks['5'][i] and j <= 2:
+        for k in range(j, j + 4):
+            next_ = k + 1
+            if cards_[k].suit != cards_[next_].suit or Card.ranks[cards_[k].rank][i] != \
+                    Card.ranks[cards_[next_].rank][i] + 1:
+                j = next_
+                break
+        else:
+            return cards_[j:j + 5], 'Straight flush'
+    return None
+
+
 def seek_hand(cards_):
     # Straight flush 검사
-    a_highest = sorted(cards_, key=lambda card_: (Card.ranks[card_.rank][-1], card_.suit), reverse=True)
-    cards_deque = deque(a_highest)
-    while Card.ranks[cards_deque[0].rank][-1] > Card.ranks['5'][-1] and len(cards_deque) >= 5:
-        hand_ = [cards_deque.popleft()]
-        for _ in range(len(cards_deque)):
-            card = cards_deque.popleft()
-            if Card.ranks[card.rank][-1] == Card.ranks[hand_[-1].rank][-1] - 1 and card.suit == hand_[-1].suit:
-                hand_.append(card)
-                if len(hand_) == 5:
-                    return hand_, 'Straight flush'
-            else:
-                cards_deque.append(card)
-
-    # Lowest straight flush 검사
-    a_lowest = sorted(a_highest, key=lambda card_: (Card.ranks[card_.rank][0], card_.suit), reverse=True)
-    cards_deque = deque(a_lowest)
-    while cards_deque[0].rank == '5' and len(cards_deque) >= 5:
-        hand_ = [cards_deque.popleft()]
-        for _ in range(len(cards_deque)):
-            card = cards_deque.popleft()
-            if Card.ranks[card.rank][0] == Card.ranks[hand_[-1].rank][0] - 1 and card.suit == hand_[-1].suit:
-                hand_.append(card)
-                if len(hand_) == 5:
-                    return hand_, 'Straight flush'
-            else:
-                cards_deque.append(card)
+    suit_highest, i = sorted(cards_, key=lambda card_: (card_.suit, Card.ranks[card_.rank][-1]), reverse=True), 0
+    while Card.ranks[suit_highest[i].rank][-1] > Card.ranks['5'][-1] and i <= 2:
+        for k in range(i, i + 4):
+            next_ = k + 1
+            if suit_highest[k].suit != suit_highest[next_].suit or Card.ranks[suit_highest[k].rank][-1] != \
+                    Card.ranks[suit_highest[next_].rank][-1] + 1:
+                i = next_
+                break
+        else:
+            return suit_highest[i:i + 5], 'Straight flush'
+    suit_lowest, i = sorted(cards_, key=lambda card_: (card_.suit, Card.ranks[card_.rank][-1]), reverse=True), 0
+    while Card.ranks[suit_lowest[i].rank][-1] <= Card.ranks['5'][-1] and i <= 2:
+        for k in range(i, i + 4):
+            next_ = k + 1
+            if suit_lowest[k].suit != suit_lowest[next_].suit or Card.ranks[suit_lowest[k].rank][-1] != \
+                    Card.ranks[suit_lowest[next_].rank][-1] + 1:
+                i = next_
+                break
+        else:
+            return suit_lowest[i:i + 5], 'Straight flush'
 
     # Four of a kind 검사
-    cards_deque = deque(a_highest)
-    while len(cards_deque) >= 5:
-        hand_ = [cards_deque.popleft()]
-        for _ in range(len(cards_deque)):
-            card = cards_deque.popleft()
-            if Card.ranks[card.rank][0] == Card.ranks[hand_[-1].rank][0]:
-                hand_.append(card)
-            else:
-                cards_deque.append(card)
-        if len(hand_) == 4:
-            hand_.append(cards_deque.popleft())
-            return hand_, 'Four of a kind'
-
-    # Full house 검사
-    cards_deque = deque(a_highest)
-    while len(cards_deque) >= 5:
-        hand_ = [cards_deque.popleft()]
-        for _ in range(len(cards_deque)):
-            card = cards_deque.popleft()
-            if Card.ranks[card.rank][0] == Card.ranks[hand_[-1].rank][0]:
-                hand_.append(card)
-            else:
-                cards_deque.append(card)
-        if len(hand_) == 4:
-            hand_.append(cards_deque.popleft())
-            return hand_, 'Full house'
-
-    # Flush 검사
-    # 정렬 상태 2개만 쓰기 ((A가 높을 때, 문양), (A가 낮을 때, 문양))
-    # deque와 pop, append 방식으로 검사
 
     return None, None
 
@@ -106,7 +84,7 @@ while True:
         cards = hole + community
         t = time.perf_counter_ns()
         hand, name = seek_hand(cards)
-        if name == 'Full house':
+        if name == 'Straight flush':
             print(time.perf_counter_ns() - t)
             print(cards, hand, name)
             break
